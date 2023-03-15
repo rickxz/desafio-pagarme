97a3d5d07895f515fca1cc9ddeacb39619afb4a9
rickxz <herick.victor.rodrigues@hotmail.com>
2023-03-14 21:20:33 -0300

    :sparkles: feat: insert transaction factory

---
+++ src/tests/utils/factories.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { TransactionFactory } from "./factories";
+
+test('generate a transaction factory with an overrided property', () => {
+    const transaction = TransactionFactory({
+        paymentMethod: 'debit_card'
+    })
+
+    expect(transaction.paymentMethod).toBe('debit_card')
+})
---
+++ src/tests/utils/factories.ts
@@ -0,0 +1,19 @@
+import { Transaction, TransactionProps } from "../../entities/transaction";
+import { getFutureDate } from "./get-future-date";
+
+type TransactionFactoryOverrides = Partial<TransactionProps>;
+
+export const TransactionFactory = (overrides?: TransactionFactoryOverrides): Transaction => {
+  const defaultProps: TransactionProps = {
+    value: 100,
+    description: 'Test transaction',
+    paymentMethod: 'credit_card',
+    cardNumber: '1234 5678 9012 3456',
+    cardholder: 'John Doe',
+    validity: getFutureDate('2028-03-15'),
+    cvv: 123,
+  };
+
+  const props = { ...defaultProps, ...overrides };
+  return new Transaction(props);
+};
