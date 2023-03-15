2bce3e29bcb3ea2a6ffb3dbaef2e1829e15e4259
rickxz <herick.victor.rodrigues@hotmail.com>
2023-03-14 21:27:56 -0300

    :hammer: refactor: use factory in transaction entity unit tests

--- src/entities/transaction.spec.ts
+++ src/entities/transaction.spec.ts
@@ -2,88 +2,41 @@ import { it, expect } from 'vitest'
 import { Transaction } from './transaction'
 
 import { getFutureDate } from '../tests/utils/get-future-date'
+import { TransactionFactory } from '../tests/utils/factories'
 
 it('should create a transaction', () => {
-    const transaction = new Transaction({
-        value: 100,
-        description: 'amazing product',
-        paymentMethod: 'debit_card',
-        cardNumber: '1234567890',
-        cardholder: 'john doe',
-        validity: getFutureDate('2028-03-15'),
-        cvv: 123
-    })
+    const transaction = TransactionFactory()
 
     expect(transaction).toBeInstanceOf(Transaction)
 })
 
 it('should not create a transaction with value lower or equal to zero', () => {
     expect(() => {
-        new Transaction({
-            value: 0,
-            description: 'amazing product',
-            paymentMethod: 'debit_card',
-            cardNumber: '1234567890',
-            cardholder: 'john doe',
-            validity: getFutureDate('2022-03-14'),
-            cvv: 123
-        })
+        TransactionFactory({ value: 0 })
     }).toThrow()
 
     expect(() => {
-        new Transaction({
-            value: -1,
-            description: 'amazing product',
-            paymentMethod: 'debit_card',
-            cardNumber: '1234567890',
-            cardholder: 'john doe',
-            validity: getFutureDate('2022-03-14'),
-            cvv: 123
-        })
+        TransactionFactory({ value: -1 })
     }).toThrow()
 })
 
 it('should not create a transaction with a payment method different from "debit_card" or "credit_card"', () => {
     expect(() => {
-        new Transaction({
-            value: 100,
-            description: 'amazing product',
-            paymentMethod: 'debit',
-            cardNumber: '1234567890',
-            cardholder: 'john doe',
-            validity: getFutureDate('2022-03-14'),
-            cvv: 123
-        })
+        TransactionFactory({ paymentMethod: 'debit' })
     }).toThrow()
 })
 
 it('should not create a transaction with a past validity date', () => {
+    const past_date = new Date()
+    past_date.setFullYear(past_date.getFullYear() - 1)
+    
     expect(() => {
-        const past_date = new Date()
-        past_date.setFullYear(past_date.getFullYear() - 1)
-
-        new Transaction({
-            value: 100,
-            description: 'amazing product',
-            paymentMethod: 'debit_card',
-            cardNumber: '1234567890',
-            cardholder: 'john doe',
-            validity: past_date,
-            cvv: 123
-        })
+        TransactionFactory({ validity: past_date })
     }).toThrow()
 })
 
 it('should save and return only the 4 last digits of the credit card', () => {
-    const transaction = new Transaction({
-        value: 100,
-        description: 'amazing product',
-        paymentMethod: 'debit_card',
-        cardNumber: '1234567890',
-        cardholder: 'john doe',
-        validity: getFutureDate('2028-03-15'),
-        cvv: 123
-    })
+    const transaction = TransactionFactory({ cardNumber: '1234 5678 9012 3456'})
 
-    expect(transaction.cardNumber).toBe('7890')
+    expect(transaction.cardNumber).toBe('3456')
 })
