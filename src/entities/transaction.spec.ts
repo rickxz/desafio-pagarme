25ee27e90ecc78494b16e6a0ab84630e48c7af08
rickxz <herick.victor.rodrigues@hotmail.com>
2023-03-14 21:19:53 -0300

    :sparkles: feat: finish transaction entity unit tests

--- src/entities/transaction.spec.ts
+++ src/entities/transaction.spec.ts
@@ -10,7 +10,7 @@ it('should create a transaction', () => {
         paymentMethod: 'debit_card',
         cardNumber: '1234567890',
         cardholder: 'john doe',
-        validity: getFutureDate('2022-03-14'),
+        validity: getFutureDate('2028-03-15'),
         cvv: 123
     })
 
@@ -18,7 +18,6 @@ it('should create a transaction', () => {
 })
 
 it('should not create a transaction with value lower or equal to zero', () => {
-
     expect(() => {
         new Transaction({
             value: 0,
@@ -42,4 +41,49 @@ it('should not create a transaction with value lower or equal to zero', () => {
             cvv: 123
         })
     }).toThrow()
+})
+
+it('should not create a transaction with a payment method different from "debit_card" or "credit_card"', () => {
+    expect(() => {
+        new Transaction({
+            value: 100,
+            description: 'amazing product',
+            paymentMethod: 'debit',
+            cardNumber: '1234567890',
+            cardholder: 'john doe',
+            validity: getFutureDate('2022-03-14'),
+            cvv: 123
+        })
+    }).toThrow()
+})
+
+it('should not create a transaction with a past validity date', () => {
+    expect(() => {
+        const past_date = new Date()
+        past_date.setFullYear(past_date.getFullYear() - 1)
+
+        new Transaction({
+            value: 100,
+            description: 'amazing product',
+            paymentMethod: 'debit_card',
+            cardNumber: '1234567890',
+            cardholder: 'john doe',
+            validity: past_date,
+            cvv: 123
+        })
+    }).toThrow()
+})
+
+it('should save and return only the 4 last digits of the credit card', () => {
+    const transaction = new Transaction({
+        value: 100,
+        description: 'amazing product',
+        paymentMethod: 'debit_card',
+        cardNumber: '1234567890',
+        cardholder: 'john doe',
+        validity: getFutureDate('2028-03-15'),
+        cvv: 123
+    })
+
+    expect(transaction.cardNumber).toBe('7890')
 })
--- src/entities/transaction.ts
+++ src/entities/transaction.ts
@@ -1,4 +1,4 @@
-type TransactionProps = {
+export type TransactionProps = {
     value: number
     description: string
     paymentMethod: string
@@ -12,11 +12,32 @@ export class Transaction {
     private props: TransactionProps
 
     constructor(props: TransactionProps) {
+        const { cardNumber } = props
+
         this.props = props
+        this.props.cardNumber = cardNumber.slice(-4)
 
         if (this.props.value <= 0) {
             throw new Error('value must be greather than zero')
         }
+        
+        const paymentMethods = ['debit_card', 'credit_card']
+
+        if (!(paymentMethods.includes(this.props.paymentMethod))) {
+            throw new Error('invalid payment method')
+        }
+
+        if (this.props.validity <= new Date()) {
+            throw new Error('expired card')
+        }
+    }
+
+    get cardNumber() {
+        return this.props.cardNumber
+    }
+
+    get paymentMethod() {
+        return this.props.paymentMethod
     }
 }
 
