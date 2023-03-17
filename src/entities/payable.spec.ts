6453fa52bac00a3d34234ef0a4fd26a4261df889
rickxz <herick.victor.rodrigues@hotmail.com>
2023-03-17 20:45:49 -0300

    :heavy_check_mark: test: end payable entity unit tests

--- src/entities/payable.spec.ts
+++ src/entities/payable.spec.ts
@@ -6,7 +6,29 @@ describe('Payable Entity Unit Tests', () => {
   test('create a payable', () => {
     const payable = PayableFactory()
 
-    console.log(payable)
     expect(payable).toBeInstanceOf(Payable)
+  })
+
+  test('payable created from a debit card transaction should have status paid, payment date D+0 and fee of 0.03', () => {
+    const payable = PayableFactory({
+      paymentMethod: 'debit_card'
+    })
+
+    expect(payable.props.status).toBe('paid')
+    expect(payable.props.paymentDate).toEqual(new Date())
+    expect(payable.props.fee).toBe(0.03)
+  })
+
+  test('payable created from a credit card transaction should have status waiting_funds, payment date D+30 and fee of 0.05', () => {
+    const future = new Date()
+    future.setDate(future.getDate() + 30)
+
+    const payable = PayableFactory({
+      paymentMethod: 'credit_card'
+    })
+
+    expect(payable.props.status).toBe('waiting_funds')
+    expect(payable.props.paymentDate.toDateString()).toEqual(future.toDateString())
+    expect(payable.props.fee).toBe(0.05)
   })
 })
