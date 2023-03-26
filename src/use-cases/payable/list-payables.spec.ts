7493dd54e63270d2700f0db0249bf0d3f4939207
rickxz <herick.victor.rodrigues@hotmail.com>
2023-03-25 22:02:57 -0300

    :white_check_mark: test: make sure to return a list of payables

--- src/use-cases/payable/list-payables.spec.ts
+++ src/use-cases/payable/list-payables.spec.ts
@@ -1,5 +1,6 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import { InMemoryPayablesRepository } from '../../repositories/payables/in-memory-payables-repository'
+import { TransactionFactory } from '../../tests/utils/factories'
 import { CreatePayable } from './create-payable'
 import { ListPayables } from './list-payables'
 
@@ -17,5 +18,16 @@ describe('List Payables', () => {
   it('should return empty array if there are no payables', async () => {
     const payables = await listPayables.execute(payablesRepository)
     expect(payables).toHaveLength(0)
+  })
+
+  it('should return a list of payables', async () => {
+    const transaction1 = TransactionFactory()
+    const transaction2 = TransactionFactory({ paymentMethod: 'debit_card' })
+
+    const payable1 = await createPayable.execute(transaction1)
+    const payable2 = await createPayable.execute(transaction2)
+
+    const payables = await listPayables.execute(payablesRepository)
+    expect(payables).toEqual([payable1, payable2])
   })
 })
